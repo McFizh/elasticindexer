64cbf1f3b02040a2c81eaa840d011af6eeb9ac8c
Pekka Harjamäki <spamtrap_github@mcfish.org>
2017-10-16 17:30:06 +0000

    Added host and port configuration

--- Model/Indexer/IndexerHandler.php
+++ Model/Indexer/IndexerHandler.php
@@ -10,57 +10,95 @@ use Magento\Framework\Search\Request\Dimension;
 use Magento\Framework\Search\Request\IndexScopeResolverInterface;
 use Magento\Framework\Indexer\SaveHandler\Batch;
 use Magento\Framework\Indexer\ScopeResolver\IndexScopeResolver;
- 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+use Elasticsearch\ClientBuilder;
+
 class IndexerHandler implements IndexerInterface
 {
     private $indexStructure;
- 
     private $data;
- 
     private $fields;
- 
     private $resource;
- 
     private $batch;
- 
     private $eavConfig;
- 
     private $batchSize;
- 
     private $indexScopeResolver;
- 
-    public function __construct(
-        Batch $batch,
-        array $data,
-        $batchSize = 50
-    ) {
+    private $scopeConfig;
+    private $elasticClient; 
+
+    public function __construct(Batch $batch, ScopeConfigInterface $scopeConfig, array $data, $batchSize = 50 ) {
         $this->batch = $batch;
         $this->data = $data;
         $this->batchSize = $batchSize;
+        $this->scopeConfig = $scopeConfig;
+
+        $t_elasticHost = [
+            "host" => $scopeConfig->getValue('catalog/search/eindexer_host', \Magento\Store\Model\ScopeInterface::SCOPE_STORE),
+            "port" => $scopeConfig->getValue('catalog/search/eindexer_port', \Magento\Store\Model\ScopeInterface::SCOPE_STORE)
+        ];        
+
+        echo "Create....\n";
+
+        $this->elasticClient = ClientBuilder::create()
+            ->setHosts($t_elasticHost)
+            ->build();
     }
  
     public function saveIndex($dimensions, \Traversable $documents)
     {
-        foreach ($this->batch->getItems($documents, $this->batchSize) as $batchDocuments) {
- 
+        foreach($dimensions as $dimension) {
+            if( ($scope = $this->parseScope($dimension)) === false)
+                continue;
+                
+
+            foreach ($this->batch->getItems($documents, $this->batchSize) as $batchDocuments) {
+                echo "!save!\n";
+            }
         }
     }
  
     public function deleteIndex($dimensions, \Traversable $documents)
     {
-        foreach ($this->batch->getItems($documents, $this->batchSize) as $batchDocuments) {
- 
+        foreach($dimensions as $dimension) {
+            if( ($scope = $this->parseScope($dimension)) === false)
+                continue;
+
+            foreach ($this->batch->getItems($documents, $this->batchSize) as $batchDocuments) {
+                echo "!delete!\n";
+            }
         }
     }
- 
+
     public function cleanIndex($dimensions)
     {
- 
+        foreach($dimensions as $dimension) {
+            if( ($scope = $this->parseScope($dimension)) === false)
+                continue;
+
+            echo "!clean!\n";
+        }
     }
  
     public function isAvailable()
     {
-        return true;
+        $resp = $this->elasticClient->ping();
+
+        if($resp)
+            return true;
+
+        return false;
+    }
+
+    //
+    private function parseScope($dimension) {
+        if($dimension->getName() != "scope") {
+            echo "Unknown dimension name/value pair.. \n";
+            echo "Dimension name:".$dimension->getName()." value:".$dimension->getValue()."\n";
+            return false;    
+        }
+
+        return $dimension->getValue();
     }
 }
 
